2826b93a6587bec837218412657b2df03db29305
hellyssoncavalcanti <hellyssoncavalcanti@gmail.com>
2026-03-22 10:33:01 -0300

    Update sync_github_settings.ps1

--- Scripts/sync_github_settings.ps1
+++ Scripts/sync_github_settings.ps1
@@ -2,8 +2,9 @@
 # Na máquina Windows de produção este arquivo pode ser personalizado localmente,
 # porque o Scripts\sync_github.ps1 o trata como protegido e não o sobrescreve.
 
-$githubToken         = 'COLE_SEU_TOKEN_AQUI'
-$ghUser              = 'seu_usuario_ou_org'
+
+$githubToken         = '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'
+$ghUser              = 'hellyssoncavalcanti'
 $repo                = 'chatGPT_Simulator'
 $branch              = 'main'
 $localDir            = 'C:\chatgpt_simulator'
@@ -11,3 +12,7 @@ $taskName            = 'chatGPT_Simulator_AutoSync'
 $syncIntervalMinutes = 10
 $chatProcessPattern  = 'Scripts\\main.py'
 $analyzerPattern     = 'Scripts\\analisador_prontuarios.py'
+
+
+#Chave de API para atualizar o PHP remotamente
+$remotePhpApiKey     = 'CVAPI_2b9c80c2abf94a76baf8b3e68d89cb7e'
